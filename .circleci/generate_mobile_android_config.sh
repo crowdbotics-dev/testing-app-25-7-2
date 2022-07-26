37edc314523e2b504ff676ba006ceb8657189e0e
leandrobono <leandrobono85@gmail.com>
2022-07-26 12:04:28 -0300

    Update generate_mobile_android_config.sh

--- .circleci/generate_mobile_android_config.sh
+++ .circleci/generate_mobile_android_config.sh
@@ -108,6 +108,13 @@ jobs:
             echo "$ANDROID_KEYSTORE" | base64 --decode > upload-key.keystore
 
       - run:
+          name: Add key.json file
+          working_directory: android
+          command:  |
+            echo "$GOOGLE_PLAY_CONSOLE_API_KEY" > key.json
+            cat key.json
+
+      - run:
           name: Build and upload to appetize.io
           command: bundle exec fastlane deploy_appetize
           working_directory: android
